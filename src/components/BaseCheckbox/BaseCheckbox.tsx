53ab11bd79bd6500a0a01880dd56b1d47a660d2d
amin-erfanian <de.amin.erfanian@gmail.com>
2025-08-05 00:52:15 +0330

    feat: implement a simple checkbox

--- src/components/BaseCheckbox/BaseCheckbox.tsx
+++ src/components/BaseCheckbox/BaseCheckbox.tsx
@@ -1,5 +1,32 @@
-const BaseCheckbox = () => {
-  return <>BaseCheckbox</>;
+import React from "react";
+import styles from "./BaseCheckbox.module.scss";
+
+type BaseCheckboxProps = React.InputHTMLAttributes<HTMLInputElement> & {
+  name: string;
+  checked?: boolean;
+  children?: React.ReactNode;
 };
 
+const BaseCheckbox = React.forwardRef<HTMLInputElement, BaseCheckboxProps>(
+  ({ name, checked, children, ...rest }, ref) => {
+    return (
+      <div className={styles["check-box"]}>
+        <input
+          ref={ref}
+          className={styles["check-box__icon-check"]}
+          type="checkbox"
+          name={name}
+          id={name}
+          checked={checked}
+          {...rest}
+        />
+        <label htmlFor={name} className={styles["check-box__label"]}>
+          {children}
+        </label>
+      </div>
+    );
+  }
+);
+
+BaseCheckbox.displayName = "BaseCheckbox";
 export default BaseCheckbox;
