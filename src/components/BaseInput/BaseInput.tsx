392405d28d77d6a0a01e61d5885e07527c5a1d69
amin-erfanian <de.amin.erfanian@gmail.com>
2025-08-04 23:50:38 +0330

    feat: complete base input, handle ref forward to use register from react hook form

--- src/components/BaseInput/BaseInput.tsx
+++ src/components/BaseInput/BaseInput.tsx
@@ -1,5 +1,78 @@
-const BaseInput = () => {
-  return <>BaseInput</>;
+import React from "react";
+import classNames from "classnames";
+import styles from "./BaseInput.module.scss";
+
+type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
+  name: string;
+  variant?: "outlined" | "text";
+  placeholder?: string;
+  prependIcon?: React.ReactNode;
+  appendIcon?: React.ReactNode;
+  error?: string;
+  type?: string;
+  dir?: "rtl" | "ltr";
+  disabled?: boolean;
 };
 
-export default BaseInput;
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  (
+    {
+      name,
+      variant = "text",
+      prependIcon,
+      appendIcon,
+      error,
+      type = "text",
+      dir = "ltr",
+      disabled = false,
+      ...rest
+    },
+    ref
+  ) => {
+    return (
+      <div
+        className={classNames(styles.input, styles[`input--${variant}`], {
+          [styles["input--error"]]: !!error,
+          [styles["input--disabled"]]: disabled,
+        })}
+      >
+        {prependIcon && (
+          <span
+            className={classNames(
+              styles.input__icon,
+              styles["input__icon--prepend"]
+            )}
+          >
+            {prependIcon}
+          </span>
+        )}
+
+        <input
+          id={name}
+          name={name}
+          type={type}
+          className={styles.input__field}
+          dir={dir}
+          disabled={disabled}
+          ref={ref}
+          {...rest}
+        />
+
+        {appendIcon && (
+          <span
+            className={classNames(
+              styles.input__icon,
+              styles["input__icon--append"]
+            )}
+          >
+            {appendIcon}
+          </span>
+        )}
+
+        {error && <div className={styles.input__errorMessage}>{error}</div>}
+      </div>
+    );
+  }
+);
+
+export default Input;
