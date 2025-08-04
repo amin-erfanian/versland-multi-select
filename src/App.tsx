6211c80c0f97b6be2e828160a1dc49f6c828d790
amin-erfanian <de.amin.erfanian@gmail.com>
2025-08-04 23:50:53 +0330

    feat: use base input inside playground

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+import BaseInput from "./components/BaseInput/BaseInput";
+import styles from "./App.module.scss";
+
+import { FieldValues, useForm } from "react-hook-form";
+
 function App() {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (formValues: FieldValues) => console.log(formValues);
+
   return (
-    <>
-      <div className="card">Hello World!</div>
-    </>
+    <div className={styles.demo}>
+      <form onSubmit={handleSubmit(onSubmit)} className={styles.demo__form}>
+        <BaseInput
+          {...register("item")}
+          placeholder="enter something.."
+          error="Some Error!"
+          variant="text"
+        />
+      </form>
+    </div>
   );
 }
 
